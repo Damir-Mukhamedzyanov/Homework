4266e257e91072039a6853640138e9c12fc42366
Damir-Mukhamedzyanov <mainboom6@gmail.com>
2025-05-12 19:35:57 +0300

    Lesson 103

---
+++ modul_2_js_1/script.js
@@ -0,0 +1,37 @@
+let obj = {
+    name: Damir,
+    age: 18,
+    country: Russia,
+    sayHello(n) {
+        return `Hello ${n}`
+    }
+}
+
+// ======================================================
+
+let users = [
+    {
+        name: 'Ivan',
+        isAdmin: false
+    },
+    {
+        name: 'Dima',
+        isAdmin: true
+    },
+    {
+        name: 'Gena',
+        isAdmin: true
+    },
+    {
+        name: 'Alex',
+        isAdmin: true
+    },
+    {
+        name: 'Arthur',
+        isAdmin: false
+    },
+    {
+        name: 'Igor',
+        isAdmin: true
+    },
+]
